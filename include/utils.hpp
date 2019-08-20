64a4d66756c69a99fea299cd4e07c74ffc55ca49
Maxime Pinard <maxime.pin@live.fr>
2019-08-20 09:47:38 +0200

    Added nbits and init_val constructor benchmarks

---
+++ include/utils.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2019 Maxime Pinard
+//
+// Distributed under the MIT license
+// See accompanying file LICENSE or copy at
+// https://opensource.org/licenses/MIT
+//
+#ifndef DYNAMIC_BITSET_BENCHMARKS_UTILS_HPP
+#define DYNAMIC_BITSET_BENCHMARKS_UTILS_HPP
+
+#include <type_traits>
+#include <climits>
+
+template<typename T>
+constexpr size_t bits_number = sizeof(T) * CHAR_BIT;
+
+template<typename T>
+constexpr bool bit_value(T value, size_t bit_pos)
+{
+	static_assert(std::is_unsigned<T>::value, "T is not an unsigned integral type");
+	assert(bit_pos < bits_number<T>);
+	return (value & (T(1) << bit_pos)) != T(0);
+}
+
+#endif //DYNAMIC_BITSET_BENCHMARKS_UTILS_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,16 @@
 // See accompanying file LICENSE or copy at
 // https://opensource.org/licenses/MIT
 //
+#include <cstring>
+#include <random>
+
 #include <benchmark/benchmark.h>
 #include <dynamic_bitset.hpp>
+
+#include "utils.hpp"
+
+constexpr std::minstd_rand::result_type SEED = 314159;
+//const std::minstd_rand::result_type SEED = std::random_device{}();
 
 template<typename T>
 void constructor_default(benchmark::State& state)
@@ -20,5 +28,22 @@ BENCHMARK_TEMPLATE(constructor_default, uint8_t);
 BENCHMARK_TEMPLATE(constructor_default, uint16_t);
 BENCHMARK_TEMPLATE(constructor_default, uint32_t);
 BENCHMARK_TEMPLATE(constructor_default, uint64_t);
+
+template<typename T>
+void constructor_nbits_initval(benchmark::State& state)
+{
+	std::mt19937 gen(SEED);
+	std::uniform_int_distribution<unsigned long long> d_value;
+	std::uniform_int_distribution<size_t> d_bits_to_take(1, bits_number<unsigned long long>);
+
+	for(auto _: state)
+	{
+		benchmark::DoNotOptimize(dynamic_bitset<T>(d_bits_to_take(gen), d_value(gen)));
+	}
+}
+BENCHMARK_TEMPLATE(constructor_nbits_initval, uint8_t);
+BENCHMARK_TEMPLATE(constructor_nbits_initval, uint16_t);
+BENCHMARK_TEMPLATE(constructor_nbits_initval, uint32_t);
+BENCHMARK_TEMPLATE(constructor_nbits_initval, uint64_t);
 
 BENCHMARK_MAIN();
