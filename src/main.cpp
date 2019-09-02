4dc91331a6cee5a8773f50fb950bc8c77e89141e
Maxime Pinard <maxime.pin@live.fr>
2019-09-02 23:18:44 +0200

    Added initializer_list constructor benchmarks

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,6 @@ void constructor_default(benchmark::State& state)
 		benchmark::DoNotOptimize(dynamic_bitset<T>());
 	}
 }
-BENCHMARK_TEMPLATE(constructor_default, uint8_t);
 BENCHMARK_TEMPLATE(constructor_default, uint16_t);
 BENCHMARK_TEMPLATE(constructor_default, uint32_t);
 BENCHMARK_TEMPLATE(constructor_default, uint64_t);
@@ -33,17 +32,54 @@ template<typename T>
 void constructor_nbits_initval(benchmark::State& state)
 {
 	std::mt19937 gen(SEED);
-	std::uniform_int_distribution<unsigned long long> d_value;
 	std::uniform_int_distribution<size_t> d_bits_to_take(1, bits_number<unsigned long long>);
+	std::uniform_int_distribution<unsigned long long> d_value;
 
 	for(auto _: state)
 	{
-		benchmark::DoNotOptimize(dynamic_bitset<T>(d_bits_to_take(gen), d_value(gen)));
+		const size_t bits_to_take = d_bits_to_take(gen);
+		const unsigned long long value = d_value(gen);
+
+		benchmark::DoNotOptimize(dynamic_bitset<T>(bits_to_take, value));
 	}
 }
-BENCHMARK_TEMPLATE(constructor_nbits_initval, uint8_t);
 BENCHMARK_TEMPLATE(constructor_nbits_initval, uint16_t);
 BENCHMARK_TEMPLATE(constructor_nbits_initval, uint32_t);
 BENCHMARK_TEMPLATE(constructor_nbits_initval, uint64_t);
+
+template<typename T>
+void constructor_initializer_list_one_value(benchmark::State& state)
+{
+	std::mt19937 gen(SEED);
+	std::uniform_int_distribution<T> d_value;
+
+	for(auto _: state)
+	{
+		const T value = d_value(gen);
+
+		benchmark::DoNotOptimize(dynamic_bitset<T>{value});
+	}
+}
+BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint16_t);
+BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint32_t);
+BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint64_t);
+
+template<typename T>
+void constructor_initializer_list_two_value(benchmark::State& state)
+{
+	std::mt19937 gen(SEED);
+	std::uniform_int_distribution<T> d_value;
+
+	for(auto _: state)
+	{
+		const T value1 = d_value(gen);
+		const T value2 = d_value(gen);
+
+		benchmark::DoNotOptimize(dynamic_bitset<T>{value1, value2});
+	}
+}
+BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint16_t);
+BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint32_t);
+BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint64_t);
 
 BENCHMARK_MAIN();
