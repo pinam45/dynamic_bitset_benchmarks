0045b8e7ee98e03b8d15482c0172d5eefb094ecf
Maxime Pinard <maxime.pin@live.fr>
2020-08-09 10:09:45 +0200

    Changed benchmarks template and added benchmark definition defines

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,46 @@
 // See accompanying file LICENSE or copy at
 // https://opensource.org/licenses/MIT
 //
-#include <cstring>
-#include <random>
+#include "utils.hpp"
 
 #include <benchmark/benchmark.h>
 #include <dynamic_bitset.hpp>
 
-#include "utils.hpp"
+#include <cstring>
+#include <random>
 
-constexpr std::minstd_rand::result_type SEED = 314159;
+static constexpr std::minstd_rand::result_type SEED = 314159;
 //const std::minstd_rand::result_type SEED = std::random_device{}();
+static constexpr size_t RANGE_START = 1ull << 2u;
+static constexpr size_t RANGE_END = 1ull << 30u;
+static constexpr size_t RANGE_MULTIPLIER = 1ull << 2u;
+#define RANGE_BENCHMARK_TEMPLATE(n, ...)  \
+	BENCHMARK_TEMPLATE(n, __VA_ARGS__)    \
+	  ->RangeMultiplier(RANGE_MULTIPLIER) \
+	  ->Range(RANGE_START, RANGE_END)
+#define DYNAMIC_BITSET_RANGE_BENCHMARK(n, b)  \
+	RANGE_BENCHMARK_TEMPLATE(n, b<uint16_t>); \
+	RANGE_BENCHMARK_TEMPLATE(n, b<uint32_t>); \
+	RANGE_BENCHMARK_TEMPLATE(n, b<uint64_t>)
+#define DYNAMIC_BITSET_BENCHMARK(n, b)  \
+	BENCHMARK_TEMPLATE(n, b<uint16_t>); \
+	BENCHMARK_TEMPLATE(n, b<uint32_t>); \
+	BENCHMARK_TEMPLATE(n, b<uint64_t>)
 
-template<typename T>
+template<typename dynamic_bitset_t>
 void constructor_default(benchmark::State& state)
 {
 	for(auto _: state)
 	{
-		benchmark::DoNotOptimize(sul::dynamic_bitset<T>());
+		benchmark::DoNotOptimize(dynamic_bitset_t());
 	}
 }
-BENCHMARK_TEMPLATE(constructor_default, uint16_t);
-BENCHMARK_TEMPLATE(constructor_default, uint32_t);
-BENCHMARK_TEMPLATE(constructor_default, uint64_t);
+DYNAMIC_BITSET_BENCHMARK(constructor_default, sul::dynamic_bitset);
 
-template<typename T>
+template<typename dynamic_bitset_t>
 void constructor_nbits_initval(benchmark::State& state)
 {
-	std::mt19937 gen(SEED);
+	std::minstd_rand gen(SEED);
 	std::uniform_int_distribution<size_t> d_bits_to_take(1, bits_number<unsigned long long>);
 	std::uniform_int_distribution<unsigned long long> d_value;
 
@@ -40,46 +53,72 @@ void constructor_nbits_initval(benchmark::State& state)
 		const size_t bits_to_take = d_bits_to_take(gen);
 		const unsigned long long value = d_value(gen);
 
-		benchmark::DoNotOptimize(sul::dynamic_bitset<T>(bits_to_take, value));
+		benchmark::DoNotOptimize(dynamic_bitset_t(bits_to_take, value));
 	}
 }
-BENCHMARK_TEMPLATE(constructor_nbits_initval, uint16_t);
-BENCHMARK_TEMPLATE(constructor_nbits_initval, uint32_t);
-BENCHMARK_TEMPLATE(constructor_nbits_initval, uint64_t);
+DYNAMIC_BITSET_BENCHMARK(constructor_nbits_initval, sul::dynamic_bitset);
 
-template<typename T>
+template<typename dynamic_bitset_t>
 void constructor_initializer_list_one_value(benchmark::State& state)
 {
-	std::mt19937 gen(SEED);
-	std::uniform_int_distribution<T> d_value;
+	using block_type = typename dynamic_bitset_t::block_type;
+	std::minstd_rand gen(SEED);
+	std::uniform_int_distribution<block_type> d_value;
 
 	for(auto _: state)
 	{
-		const T value = d_value(gen);
+		const block_type value = d_value(gen);
 
-		benchmark::DoNotOptimize(sul::dynamic_bitset<T>{value});
+		benchmark::DoNotOptimize(dynamic_bitset_t{value});
 	}
 }
-BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint16_t);
-BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint32_t);
-BENCHMARK_TEMPLATE(constructor_initializer_list_one_value, uint64_t);
+DYNAMIC_BITSET_BENCHMARK(constructor_initializer_list_one_value, sul::dynamic_bitset);
 
-template<typename T>
+template<typename dynamic_bitset_t>
 void constructor_initializer_list_two_value(benchmark::State& state)
 {
-	std::mt19937 gen(SEED);
-	std::uniform_int_distribution<T> d_value;
+	using block_type = typename dynamic_bitset_t::block_type;
+	std::minstd_rand gen(SEED);
+	std::uniform_int_distribution<block_type> d_value;
 
 	for(auto _: state)
 	{
-		const T value1 = d_value(gen);
-		const T value2 = d_value(gen);
+		const block_type value1 = d_value(gen);
+		const block_type value2 = d_value(gen);
 
-		benchmark::DoNotOptimize(sul::dynamic_bitset<T>{value1, value2});
+		benchmark::DoNotOptimize(dynamic_bitset_t{value1, value2});
 	}
 }
-BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint16_t);
-BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint32_t);
-BENCHMARK_TEMPLATE(constructor_initializer_list_two_value, uint64_t);
+DYNAMIC_BITSET_BENCHMARK(constructor_initializer_list_two_value, sul::dynamic_bitset);
+
+template<typename dynamic_bitset_t>
+void constructor_string(benchmark::State& state)
+{
+	const size_t string_size = static_cast<size_t>(state.range(0));
+	std::minstd_rand gen(SEED);
+	std::bernoulli_distribution d_bit;
+	std::string str;
+	str.reserve(string_size);
+	for(size_t i = 0; i < string_size; ++i)
+	{
+		str.push_back(d_bit(gen) ? '1' : '0');
+	}
+
+	for(auto _: state)
+	{
+		benchmark::DoNotOptimize(dynamic_bitset_t(str));
+		benchmark::ClobberMemory();
+	}
+
+	state.counters["1_char_time"] = benchmark::Counter(string_size,
+	                                                   benchmark::Counter::kIsIterationInvariantRate
+	                                                     | benchmark::Counter::kInvert,
+	                                                   benchmark::Counter::OneK::kIs1000);
+	state.counters["chars_per_second"] =
+	  benchmark::Counter(string_size,
+	                     benchmark::Counter::kIsIterationInvariantRate,
+	                     benchmark::Counter::OneK::kIs1024);
+}
+DYNAMIC_BITSET_RANGE_BENCHMARK(constructor_string, sul::dynamic_bitset);
 
 BENCHMARK_MAIN();
