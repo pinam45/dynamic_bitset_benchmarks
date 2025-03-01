9581ba02aa7252e008dc7e87fc1d08c2391d22e2
Maxime Pinard <maxime.pin@live.fr>
2025-03-01 15:41:28 +0100

    Added fix for dynamic_bitset iterate_bits_on

--- libs/fix/include/fix/dynamic_bitset.hpp
+++ libs/fix/include/fix/dynamic_bitset.hpp
@@ -101,4 +101,92 @@ namespace fix::dynamic_bitset
         }
     }
 
+    template<typename dynamic_bitset_t, typename Function, typename... Parameters>
+    constexpr void
+    do_iterate_bits_on(dynamic_bitset_t& bitset, Function&& function, Parameters&&... parameters) noexcept
+    {
+        // check
+        if constexpr(!std::is_invocable_v<Function, size_t, Parameters...>)
+        {
+            static_assert(dependent_false<Function>::value, "Function take invalid arguments");
+            // function should take (size_t, parameters...) as arguments
+        }
+
+        // if std::vector<bool>
+        if constexpr(std::is_same_v<dynamic_bitset_t, std::vector<bool>>)
+        {
+            if constexpr(std::is_same_v<std::invoke_result_t<Function, size_t, Parameters...>, void>)
+            {
+                for(const size_t i_bit: std::views::iota(0u, bitset.size()))
+                {
+                    if(bitset[i_bit])
+                    {
+                        std::invoke(std::forward<Function>(function), i_bit, std::forward<Parameters>(parameters)...);
+                    }
+                }
+            }
+            else if constexpr(std::is_convertible_v<std::invoke_result_t<Function, size_t, Parameters...>, bool>)
+            {
+                for(const size_t i_bit: std::views::iota(0u, bitset.size()))
+                {
+                    if(bitset[i_bit])
+                    {
+                        if(!std::invoke(
+                             std::forward<Function>(function), i_bit, std::forward<Parameters>(parameters)...))
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                static_assert(dependent_false<Function>::value, "Function have invalid return type");
+                // return type should be void, or convertible to bool
+            }
+        }
+        // if dynamic_bitset without iterate_bits_on
+        else if constexpr(!requires {
+                              bitset.iterate_bits_on(std::forward<Function>(function),
+                                                     std::forward<Parameters>(parameters)...);
+                          })
+        {
+            if constexpr(std::is_same_v<std::invoke_result_t<Function, size_t, Parameters...>, void>)
+            {
+                size_t i_bit = bitset.find_first();
+                // boost::dynamic_bitset::find_next() returns dynamic_bitset_t::npos
+                // std::tr2::dynamic_bitset::find_next() returns dynamic_bitset.size()
+                while(i_bit < bitset.size())
+                {
+                    std::invoke(std::forward<Function>(function), i_bit, std::forward<Parameters>(parameters)...);
+                    i_bit = bitset.find_next(i_bit);
+                }
+            }
+            else if constexpr(std::is_convertible_v<std::invoke_result_t<Function, size_t, Parameters...>, bool>)
+            {
+                size_t i_bit = bitset.find_first();
+                // boost::dynamic_bitset::find_next() returns dynamic_bitset_t::npos
+                // std::tr2::dynamic_bitset::find_next() returns dynamic_bitset.size()
+                while(i_bit < bitset.size())
+                {
+                    if(!std::invoke(std::forward<Function>(function), i_bit, std::forward<Parameters>(parameters)...))
+                    {
+                        break;
+                    }
+                    i_bit = bitset.find_next(i_bit);
+                }
+            }
+            else
+            {
+                static_assert(dependent_false<Function>::value, "Function have invalid return type");
+                // return type should be void, or convertible to bool
+            }
+        }
+        // if dynamic_bitset with iterate_bits_on
+        else
+        {
+            bitset.iterate_bits_on(std::forward<Function>(function), std::forward<Parameters>(parameters)...);
+        }
+    }
+
 } // namespace fix::dynamic_bitset
