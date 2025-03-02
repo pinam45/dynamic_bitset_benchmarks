01e64da1367b652ab3985b202aa876c9019280d0
Maxime Pinard <maxime.pin@live.fr>
2025-03-02 22:33:10 +0100

    Added OR-Library instances loading in USCP benchmarks

--- benchmarks/uscp/src/main.cpp
+++ benchmarks/uscp/src/main.cpp
@@ -6,12 +6,58 @@
 // https://opensource.org/licenses/MIT
 //
 #include <config.hpp>
+#include <global.hpp>
+#include <greedy.hpp>
+#include <rwls.hpp>
+
+#include <uscp/or_library.hpp>
+
+#include <sul/dynamic_bitset.hpp>
+#ifdef HAS_BOOST
+#    include <boost/dynamic_bitset.hpp>
+#endif
+#ifdef HAS_STD_TR2_DYNAMIC_BITSET
+#    include <tr2/dynamic_bitset>
+#endif
 
 #include <benchmark/benchmark.h>
 #include <fmt/core.h>
 #include <fmt/std.h>
 
+#include <chrono>
 #include <filesystem>
+
+bool load_instance(const std::filesystem::path& instance_path) noexcept
+{
+#define LOAD_INSTANCE_FOR(BITSET_TYPE) \
+    if(auto instance = uscp::problem::or_library::instance_from<BITSET_TYPE>(instance_path)) \
+    { \
+        fmt::print(stderr, " [✓] " #BITSET_TYPE "\n"); \
+        global::benchmark_instance<BITSET_TYPE> = *instance; \
+    } \
+    else \
+    { \
+        fmt::print(stderr, "failed load instance: {}\n", instance.error()); \
+        return false; \
+    }
+    LOAD_INSTANCE_FOR(sul::dynamic_bitset<uint16_t>);
+    LOAD_INSTANCE_FOR(sul::dynamic_bitset<uint32_t>);
+    LOAD_INSTANCE_FOR(sul::dynamic_bitset<uint64_t>);
+#ifdef HAS_BOOST
+    LOAD_INSTANCE_FOR(boost::dynamic_bitset<uint16_t>);
+    LOAD_INSTANCE_FOR(boost::dynamic_bitset<uint32_t>);
+    LOAD_INSTANCE_FOR(boost::dynamic_bitset<uint64_t>);
+#endif
+#ifdef HAS_STD_TR2_DYNAMIC_BITSET
+    LOAD_INSTANCE_FOR(std::tr2::dynamic_bitset<uint16_t>);
+    LOAD_INSTANCE_FOR(std::tr2::dynamic_bitset<uint32_t>);
+    LOAD_INSTANCE_FOR(std::tr2::dynamic_bitset<uint64_t>);
+#endif
+    LOAD_INSTANCE_FOR(std::vector<bool>);
+#undef LOAD_INSTANCE_FOR
+
+    return true;
+}
 
 int main(int argc, char** argv)
 {
@@ -51,6 +97,17 @@ int main(int argc, char** argv)
         }
         return 1;
     }
+
+    // Load the instance
+    fmt::print(stderr, "load OR-Library instance from: {}\n", instance_path);
+    if(!load_instance(instance_path))
+    {
+        return 1;
+    }
+    fmt::print(stderr,
+               "instance loaded: {} points, {} subsets\n\n",
+               global::benchmark_instance<sul::dynamic_bitset<uint16_t>>.points_number,
+               global::benchmark_instance<sul::dynamic_bitset<uint16_t>>.subsets_number);
 
     // Run benchmarks
     benchmark::RunSpecifiedBenchmarks();
